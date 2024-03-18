91b03d2ceb42043ad506b462ac9ee1b1921ae8fe
yjoon2k <syjoon2k@gmail.com>
2024-03-18 20:51:10 +0900

    backend-picnic added

---
+++ picnic/src/test/java/NotFound/picnic/PicnicApplicationTests.java
@@ -0,0 +1,13 @@
+package NotFound.picnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
